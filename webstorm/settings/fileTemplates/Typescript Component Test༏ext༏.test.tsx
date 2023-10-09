1df66c1400894ee62e2c86942a61cd0273a0e1b0
Marius Tobiassen Bungum <marius@mh2.no>
2023-10-09 13:16:10 +0200

    :wrench: Added webstorm settings

---
+++ webstorm/settings/fileTemplates/Typescript Component Test༏ext༏.test.tsx
@@ -0,0 +1,7 @@
+import { faker } from '@faker-js/faker';
+
+import { render, screen } from 'test-utils';
+
+test('this is the test', async () => {
+    render()
+});
---
+++ webstorm/settings/fileTemplates/Typescript Component stories༏ext༏.stories.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+import { Story } from '@storybook/react';
+
+export default {
+  title: 'PATH',
+  component: COMPONENT,
+  argTypes: {
+    label: { control: 'text' } 
+  },
+  args: {
+    label: 'label'
+  },
+};
+
+export const Primary: Story<PROPS> = (args) => {
+  return null
+};
